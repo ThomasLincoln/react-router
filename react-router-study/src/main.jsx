9c9d453fde39c91ff12bb3f2446fd8650776b7d1
ThomasLincoln <thmas5952@gmail.com>
2023-08-22 20:39:08 -0300

    index page

--- react-router-study/src/main.jsx
+++ react-router-study/src/main.jsx
@@ -11,6 +11,7 @@ import EditContact, { action as editAction } from "./routes/edit";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
